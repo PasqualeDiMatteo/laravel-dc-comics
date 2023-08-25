63d46bd3f2d83f296973c0b4535a168e7c1fb11f
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-25 15:30:50 +0200

    added comics on page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,4 +4,23 @@
 
 @section('main')
     <h1>Comics</h1>
+    <div class="container">
+        <div class="row row-cols-6">
+            @forelse ($comics as $comic)
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="h-100">
+                            <img src="{{ $comic->thumb }}" class="card-img-top" alt="..." class="img-fluid w-100">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <a href="#" class="btn btn-primary">Info</a>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h2>Non ci sono Comics</h2>
+            @endforelse
+        </div>
+    </div>
 @endsection
