e54303f7626a9b7744b126bb56cf12799c31981a
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-25 14:01:59 +0200

    create min layout and home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Home')
 
-    <title>Laravel</title>
+@section('main')
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <h1>Home page</h1>
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-</body>
-
-</html>
+@endsection
